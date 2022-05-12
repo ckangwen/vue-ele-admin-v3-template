0752a0179b396cae4e0754be79d805e80d7101a9
ckangwen <2437037469@qq.com>
2022-05-12 22:07:35 +0800

    feat: 新增scss全局文件 & 新增jsx、unplugin-auto-import、unplugin-vue-components插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,36 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import Windicss from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variable.scss";`,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  plugins: [vue(), Windicss()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Windicss(),
+  ],
 });
